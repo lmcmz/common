62c230b07ffc132f2cadd949daab7533880af7e7
lmcmz <lmcmze@gmail.com>
2020-03-11 01:20:02 +1100

    Replace iOS project with Swift

---
+++ ios/common/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  common
+//
+//  Created by lmcmz on 11/3/20.
+//  Copyright © 2020 DAOstack. All rights reserved.
+//
+
+import UIKit
+import React
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    private var bridge: RCTBridge?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let jsCodeLocation = URL(string: "http://localhost:8081/index.ios.bundle?platform=ios&dev=true")
+//            RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index", fallbackResource: nil)
+        bridge = RCTBridge(bundleURL: jsCodeLocation, moduleProvider: nil, launchOptions: nil)
+        
+        
+        let rootView = RCTRootView(bridge: bridge!, moduleName: "common", initialProperties: launchOptions)
+        
+        let rootViewController = UIViewController()
+        rootViewController.view = rootView
+
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = rootViewController
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
---
+++ ios/common/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  common
+//
+//  Created by lmcmz on 11/3/20.
+//  Copyright © 2020 DAOstack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
